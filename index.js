9beb1a4c01e43f50e6f80dc1f41f3f71f75d3f35
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-03 23:10:31 -0300

    Adicionando Ejs e pastas de views

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 require('dotenv').config();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (_req, resp) => {
-    resp.status(200).json('OK')
+  resp.status(200).render('../src/views/index');
 });
 
-app.listen(3000, ()=> console.log(`Online na porta ${process.env.PORT}`));
+app.listen(3000, () => console.log(`Online na porta ${process.env.PORT}`));
