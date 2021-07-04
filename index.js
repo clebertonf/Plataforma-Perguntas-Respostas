1cbcd1b6386883e19a7dba4854a6722b3fd03067
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-04 14:47:15 -0300

    Adicionando bootstrap ao projeto

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 require('dotenv').config();
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (_req, resp) => {
-  resp.status(200).render('../src/views/index');
+  resp.status(200).render('../src/views/index', {});
 });
 
 app.listen(3000, () => console.log(`Online na porta ${process.env.PORT}`));
