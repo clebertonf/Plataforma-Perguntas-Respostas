4c7da5eff0a68a34629071b56fd5b1d15d645839
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-04 17:37:48 -0300

    Adicionando partials do EJS, finalizando frontEnd de pergunta

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
 app.get('/', (_req, resp) => {
   resp.status(200).render('../src/views/index', {});
+});
+
+app.get('/pergunta', (_req, resp) => {
+  resp.status(200).render('../src/views/formPergunta.ejs');
+});
+
+app.post('/pergunta', (req, resp) => {
+  const { titulo, descricao } = req.body;
+  resp.status(201).send({ titulo, descricao });
 });
 
 app.listen(3000, () => console.log(`Online na porta ${process.env.PORT}`));
